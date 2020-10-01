2b8ae66ac53fdca9089d032873a4a608ba81d081
yechan <yyechan@naver.com>
2020-10-01 18:15:08 +0900

    add Member DTO

---
+++ src/shop/dongguktime/web/dto/MemberDTO.java
@@ -0,0 +1,52 @@
+package shop.dongguktime.web.dto;
+
+public class MemberDTO {
+
+	
+	private String id;
+	private String pw;
+	private String eMail;
+	private boolean isAuthenticated; // 학교 인증 여부 
+	
+	public MemberDTO(String id, String pw,String eMail,boolean isAuthenticated) {
+		this.id = id;
+		this.pw = pw;
+		this.eMail = eMail;
+		this.isAuthenticated = isAuthenticated;	
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public boolean isAuthenticated() {
+		return isAuthenticated;
+	}
+
+	public void setAuthenticated(boolean isAuthenticated) {
+		this.isAuthenticated = isAuthenticated;
+	}
+	
+	
+	
+}

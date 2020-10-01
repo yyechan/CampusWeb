a3d25b86c590d437c1065bcb3dda10074d9e615d
yechan <yyechan@naver.com>
2020-10-01 18:02:08 +0900

    SHA-256 generator

---
+++ src/shop/dongguktime/web/sha256/SHA256.java
@@ -0,0 +1,35 @@
+package shop.dongguktime.web.sha256;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+	
+	
+	//str ¾ÏÈ£È­
+	public String encrypt(String str) {
+		
+		String sha256 = "";
+		
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(str.getBytes());
+			byte byteData[] = md.digest();
+			StringBuffer sb = new StringBuffer();
+			for(int i = 0 ; i < byteData.length ; i++) {
+				sb.append(Integer.toString((byteData[i]&0xff) + 0x100,16).substring(1));
+			}
+			sha256 = sb.toString();
+		}catch(NoSuchAlgorithmException e) {
+			
+			System.out.println("Encrypt Error");
+			sha256 = null;
+		}
+			
+		
+		
+		return sha256;
+	}
+	
+
+}

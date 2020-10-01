3035ec1498795332d83a1b520277da102d484201
yechan <yyechan@naver.com>
2020-10-01 16:27:24 +0900

    init Front Controller

---
+++ src/shop/dongguktime/web/frontcontroller/FrontController.java
@@ -0,0 +1,58 @@
+package shop.dongguktime.web.frontcontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import shop.dongguktime.web.command.Command;
+
+/**
+ * Servlet implementation class FrontController
+ */
+@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	public FrontController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet Method");
+		actionDo(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost Method");
+		actionDo(request,response);
+	}
+	
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("actionDo Method");
+		
+		response.setCharacterEncoding("EUC-KR");
+		
+		String viewPage = null;
+		Command command = null;
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String context = uri.substring(conPath.length());
+		
+		// context에 다른 분기 
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+		
+	}
+
+}
